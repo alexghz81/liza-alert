27c0be33fc6389df2f31102b11beda1e7228e5c4
alexghz81 <alex-ghz@yandex.ru>
2022-02-15 18:51:00 +0300

    asteriks on password.html

--- script/passw.js
+++ script/passw.js
@@ -1,11 +1,7 @@
 const inputEl = document.querySelector('#pass');
-
 const dummyEl = document.querySelector('.account__password');
-
-// const resultEl = document.querySelector('#result');
 
 inputEl.addEventListener('keyup', () => {
     const dummyText = Array(inputEl.value.length).fill('*').join('');
     dummyEl.textContent = dummyText;
-    // resultEl.innerHTML = inputEl.value;
 })
