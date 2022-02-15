4a929facb4c41e49b5331d1aedba9798401bfbd8
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2022-02-15 21:13:11 +0300

    Revert "Merge branch 'develop' into fix/profile-buttons"
    This reverts commit 9b01377ef45c8e1370b9eb1a565ca1a114ceea51, reversing
    changes made to 6001f818f24be738d7d6174f51250566a8a37d21.

--- script/const.js
+++ script/const.js
@@ -1,7 +1,6 @@
 const resetButton = document.querySelector('.filters__clear-button');
 const tagsContainer = document.querySelector('.filters_tags');
 const checkboxContainer = document.querySelector('.filters__difficulty-list');
-const radioContainer = document.querySelector('.filters__status');
 const buttonsSignUp = document.querySelectorAll('.button_signup');
 const checkedTags = {};
 const saveProfileButton = document.querySelector('#profile');
